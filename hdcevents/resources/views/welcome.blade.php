1477740a7e8b12d8b8780254741af1969bae1e93
João Victor Mendes <joao.mendes.costa97@gmail.com>
2022-03-21 13:09:00 -0500

    Update
    Update CSS

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -8,5 +8,23 @@
         <input type="text" name="search" id="search" class="form-control" placeholder="Procurar">
     </form>
 </div>
+
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+        <div class="card col-md-3">
+            <img src="../img/event_placeholder.jpg" alt="{{ $event->title }}"> <!--Atri alt da img de acessibilidade-->
+            <div class="card-body">
+                <p class="card-date">01/01/2022 </p>
+                <h5 class="card-title">{{  $event->title }}</h5>
+                <p class="card-participants">X Participantes</p>
+                <a href="#" class="btn btn-primary">Saber mais</a>
+            </div>
+        </div>
+    @endforeach
+    </div>
+</div>
     
 @endsection
