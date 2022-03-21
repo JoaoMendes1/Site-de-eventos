ee62d1096b042612daf04aec62ece2e13502eb0f
João Victor Mendes <joao.mendes.costa97@gmail.com>
2022-03-21 17:22:35 -0500

    Update project
    Adicionado sistemas de upload de imagens, e mensagem de registro concluído no site.

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
     <div id="cards-container" class="row">
         @foreach($events as $event)
         <div class="card col-md-3">
-            <img src="../img/event_placeholder.jpg" alt="{{ $event->title }}"> <!--Atri alt da img de acessibilidade-->
+            <img src="/img/events/{{  $event->image }}" alt="{{ $event->title }}"> <!--Atri alt da img de acessibilidade-->
             <div class="card-body">
                 <p class="card-date">01/01/2022 </p>
                 <h5 class="card-title">{{  $event->title }}</h5>
