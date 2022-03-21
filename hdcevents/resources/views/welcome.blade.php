0348052ea0f85fa2b6542d97d59768936c1c7674
João Victor Mendes <joao.mendes.costa97@gmail.com>
2022-03-21 12:31:56 -0500

    Update
    update events

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -1,41 +1,12 @@
 @extends('layouts.main')
 @section('title', 'HDC Events')
 @section('content')
-       
-       <h1>Algum título</h1>
-        <img src="/img/banner.jpg" alt="">
-        @if(10 > 11)
-            <p>A condição é true</p>
-        @endif
-            <p>{{$nome}}</p>
 
-            @if($nome == "Pedro")
-            <p>O nome é Pedro</p>
-            @elseif($nome == "João")
-            <p>O nome é {{$nome}} e ele tem {{$idade}} anos, e trabalha como {{$profissao}} </p>
-            @else
-            <p>O nome não é Pedro</p>
-            @endif
-
-            @for($i = 0; $i < count($arr); $i++ )
-                <p>{{$arr[$i]}} - {{ $i }}</p>
-                @if($i == 2)
-                <p>O i é 2</p>
-                @endif
-            @endfor
-
-            @foreach($nomes as $nome)
-                <p>{{ $loop->index }}</p>
-                <p>{{$nome}}</p>
-            @endforeach
-
-            @php
-                $name = "João";
-                echo $name;
-            
-            @endphp
-
-            <!-- Comentário PHP -->
-            {{-- Comentário blade --}}
-
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" name="search" id="search" class="form-control" placeholder="Procurar">
+    </form>
+</div>
+    
 @endsection
--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -23,13 +23,4 @@ Route::get('/contact', function(){
     return view('contact');
 });
 
-Route::get('/produtos', function(){
 
-    $busca = request('search');
-
-    return view('products', ['busca' => $busca]);
-});
-
-Route::get('/produtos_teste/{id?}', function($id = 1){
-    return view('product', ['id' => $id]);
-});
