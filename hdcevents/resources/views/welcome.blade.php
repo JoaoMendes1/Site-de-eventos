9e37cf5fe0bb2d91fd5bc6e43761074a0a32d7e4
João Victor Mendes <joao.mendes.costa97@gmail.com>
2022-03-23 13:08:56 -0500

    update

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -17,13 +17,16 @@
         <div class="card col-md-3">
             <img src="/img/events/{{  $event->image }}" alt="{{ $event->title }}"> <!--Atri alt da img de acessibilidade-->
             <div class="card-body">
-                <p class="card-date">01/01/2022 </p>
+                <p class="card-date">{{ date('d/m/y', strtotime($event->date)) }}</p>
                 <h5 class="card-title">{{  $event->title }}</h5>
                 <p class="card-participants">X Participantes</p>
                 <a href="/events/{{  $event->id }}" class="btn btn-primary">Saber mais</a>
             </div>
         </div>
     @endforeach
+    @if(count($events) == 0)
+    <p>Não há eventos disponíveis</p>
+    @endif
     </div>
 </div>
     
