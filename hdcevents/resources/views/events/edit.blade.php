503296e315b3ce85daffb37b1bf2aad6a7495e62
João Victor Mendes <joao.mendes.costa97@gmail.com>
2022-03-27 00:09:53 -0500

    Update edit.blade.php

--- hdcevents/resources/views/events/edit.blade.php
+++ hdcevents/resources/views/events/edit.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.main')
-@section('title', 'Editar Evento')
+@section('title', 'Editando: ' . $event->title)
 @section('content')
 
 <div id="event-create-container" class="col-md-6 offset-md-3">
-    <h1>Edite o seu evento</h1>
-    <form action="/events"method="POST" enctype="multipart/form-data" >
+    <h1>Editando: {{ $event->title }}</h1>
+    <form action="/events/update/{{ $event->id }}"method="POST" enctype="multipart/form-data" >
         @csrf
+        @method('PUT')
 
     <div class="form-group">
             <label for="image">Imagem do evento:</label>
             <input type="file" id="image" name="image" class="from-control-file">
+            <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}" class="img-preview">
      </div>
         
     <div class="form-group">
             <label for="title">Evento:</label>
-            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento" value="{{ $event->title }}">
         </div>
 
     <div class="form-group">
