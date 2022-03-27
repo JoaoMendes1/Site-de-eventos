792cb16980aeb871b7014375e0035cc3130d615f
João Victor Mendes <joao.mendes.costa97@gmail.com>
2022-03-27 00:27:10 -0500

    Update

--- hdcevents/resources/views/events/edit.blade.php
+++ hdcevents/resources/views/events/edit.blade.php
@@ -21,26 +21,26 @@
 
     <div class="form-group">
         <label for="date">Data do evento:</label>
-        <input type="date" class="form-control" id="date" name="date">
+        <input type="date" class="form-control" id="date" name="date" value="{{ $event->date->format('Y-m-d') }}">
     </div>
 
         <div class="form-group">
             <label for="title">Cidade:</label>
-            <input type="text" class="form-control" id="city" name="city" placeholder="Nome da cidade">
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento" value="$event->city">
         </div>
 
         <div class="form-group">
             <label for="title">O evento é privado?</label>
             <select name="private" id="private" class="form-control">
                  <option value="0">Não</option>
-                 <option value="1">Sim</option>
+                 <option value="1" {{ $event->private == 1? "selected='selected'" : "" }}>Sim</option>
             </select>
           
         </div>
 
         <div class="form-group">
             <label for="title">Descrição:</label>
-            <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento"></textarea>
+            <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento"> {{ $event->description }}</textarea>
         </div>
 
         <div class="form-group">
