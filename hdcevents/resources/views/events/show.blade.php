fb80063a2fa7dfcb97f5473f2f99b3ba17f75081
João Victor Mendes <joao.mendes.costa97@gmail.com>
2022-03-29 23:21:25 -0500

    Update show.blade.php

--- hdcevents/resources/views/events/show.blade.php
+++ hdcevents/resources/views/events/show.blade.php
@@ -23,10 +23,10 @@
                     </a>
                 </form>
                 @else
-                    <p class="already-joined-msg">Você já está participando deste evento!</p>
+                    <p class="already-joined-msg">Erro! Você já está participando deste evento!</p>
                 @endif
             
-                <h3>O evento conta com o:</h3>
+                <h3>O evento conta com:</h3>
                 <ul id="items-list">
                     @foreach($event->items as $item)
                     <li><ion-icon name="play-outline"></ion-icon> <span>{{  $item }}</span></li>
